5365df553f0500c27aad39735d6f02b1def8627e
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-08 05:23:54 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,16 @@
 # Your code goes here!
 class Anagram
-  attr_accessor :word
-
-  def initialize(word)
-    @word = word
+  def initialize(anagram)
+    @anagram = anagram
   end
 
-  def match(array)
+  def match(words)
+    words.select do |word|
+      is_anagram?(word)
+    end
+  end
+
+  def is_anagram?(word)
+    word.chars.sort == @anagram.chars.sort
   end
 end
