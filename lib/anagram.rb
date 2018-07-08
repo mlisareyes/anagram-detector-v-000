e66e35fd99feb8661f2ff6729fdfa1180a8cb974
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-08 04:53:57 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,14 +5,4 @@ class Anagram
   def initialize(word)
     @word = word
   end
-
-  def match(words)
-    words.select do |word|
-      is_anagram?(word)
-    end
-  end
-
-  def is_anagram?(word)
-    word.chars.sort == @anagram.chars.sort
-  end
 end
